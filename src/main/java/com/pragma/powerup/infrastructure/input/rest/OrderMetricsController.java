2ae204184616aab0a9b335dd48f9aa09f4d6fd02
Brayan Barco <b.barco@nelumbo.com.co>
2025-12-02 17:32:45 -0500

    definition of metrics-endpoints by calculate efficiency of the orders and employees

---
+++ src/main/java/com/pragma/powerup/infrastructure/input/rest/OrderMetricsController.java
@@ -0,0 +1,25 @@
+package com.pragma.powerup.infrastructure.input.rest;
+
+import com.pragma.powerup.apifirst.api.MetricsApi;
+import com.pragma.powerup.apifirst.model.EmployeeEfficiencyMetricsResponseDto;
+import com.pragma.powerup.apifirst.model.OrdersDurationMetricsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.OffsetDateTime;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderMetricsController implements MetricsApi {
+
+    @Override
+    public ResponseEntity<EmployeeEfficiencyMetricsResponseDto> getEmployeeEfficiencyMetrics(Long restaurantId, OffsetDateTime startDate, OffsetDateTime endDate, Integer minOrdersCompleted, Integer page, Integer size, String sortBy, String sortDirection) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<OrdersDurationMetricsResponseDto> getOrdersDurationMetrics(Long restaurantId, OffsetDateTime startDate, OffsetDateTime endDate, Integer page, Integer size, String sortBy, String sortDirection) {
+        return null;
+    }
+}
